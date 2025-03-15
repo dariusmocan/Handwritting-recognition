ec800015cf6817ff3d708beede2078e953778e49
dariusmocan <dariusmocan2003@gmail.com>
2025-03-15 22:01:22 +0200

    studiere eficienta

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+# plt.imshow(x_train[0], cmap="gray")
+# plt.show()
+
 # model = tf.keras.models.Sequential()
 # model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 # model.add(tf.keras.layers.Dense(128, activation='relu'))
@@ -21,7 +24,7 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 #               loss='sparse_categorical_crossentropy',
 #               metrics=['accuracy'])
 
-# model.fit(x_train, y_train, epochs=3)
+# model.fit(x_train, y_train, epochs=5)
 
 # model.save('handwritten.keras')
 
@@ -30,17 +33,19 @@ model = load_model('handwritten.keras')
 
 
 # loss, accuracy = model.evaluate(x_test, y_test)
-
 # print(loss)
 # print(accuracy)
 
 
 
+# i should invert the image to make it look like the training data
+
 image_number = 1
 while os.path.isfile(f"digits/digit{image_number}.png"):
     try:
-        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = cv2.imread(f"digits/digit{image_number}.png",cv2.IMREAD_GRAYSCALE)
         img = np.invert(np.array([img]))
+        img = img/255.0
         plt.imshow(img[0], cmap=plt.cm.binary)
         prediction = model.predict(img)
         print(f"The number is probably: {np.argmax(prediction)}")
@@ -49,3 +54,16 @@ while os.path.isfile(f"digits/digit{image_number}.png"):
         print(e)
     finally:
         image_number += 1
+
+
+
+#am testat cum functioneaza pe imagine alb pe negru
+
+img = cv2.imread("digits/white1.png",cv2.IMREAD_GRAYSCALE)
+img= cv2.resize(img, (28, 28))
+img = img/255.0
+img = np.array([img])
+plt.imshow(img[0], cmap="gray")
+prediction = model.predict(img)
+print(f"The number is probably: {np.argmax(prediction)}")
+plt.show()
